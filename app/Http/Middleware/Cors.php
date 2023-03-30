6af8ac2299d7506d06d4aeb8db2e7ca4fe9bcd96
AimFried <julien40.der@gmail.com>
2023-03-30 12:09:57 +0200

    fix cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,9 +1,9 @@
 <?php
-
+ 
 namespace App\Http\Middleware;
-
+ 
 use Closure;
-
+ 
 class Cors
 {
     /**
@@ -16,7 +16,10 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*');
-
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', true)
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+            ->header('Accept', 'application/json');
     }
 }
